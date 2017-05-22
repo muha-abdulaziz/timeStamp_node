30ba993df2361ad54fb147e1480d308ac14db8bd
muha-abdulaziz <muha.abdulaziz@gmail.com>
2017-05-22 09:06:32 +0200

    add the time stamp logic

---
+++ timeStamp.js
@@ -0,0 +1,45 @@
+//Not a Date Error.
+NaD = new Error("Not a Valid Date.");
+
+var months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December"
+}
+
+function checkDate(aDate) {
+    var date = new Date(aDate);
+    if (date == "Invalid Date") return "";
+    
+    return date;
+};
+
+function timeStampe (aDate) {
+    var date = checkDate(aDate);
+    result = {}
+    if (!date) {
+        result.unix = null;
+        result.natural = null;
+    } else {
+        var day = date.getDate();
+        var month = date.getMonth();
+        var monthName = months[month];
+        var year = date.getFullYear();
+
+        result.unix = date.getTime();
+        result.natural = monthName + " " + day + "," + year;
+
+        return result;
+    }
+}
+
+module.exports = timeStampe;
